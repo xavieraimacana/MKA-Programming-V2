fdd398e5bc66f28e3b18f6cc8f79ea41a1220846
Kevin Alban <albankevin680@gmail.com>
2026-06-09 18:22:21 -0500

    feat(equipment): implement Equipment status control and maintenance logger [v0.3.3]

---
+++ 06Code/CoffeeShop/src/main/java/ec/espe/edu/coffeeshop/model/EquipmentStatus.java
@@ -0,0 +1,12 @@
+package ec.espe.edu.coffeeshop.model;
+
+/**
+ * Enumerates the possible states of coffee shop equipment.
+ * 
+ * @author Kevin Albán, MKA Programmer, @ESPE
+ */
+public enum EquipmentStatus {
+    OPERATIONAL,
+    MAINTENANCE_REQUIRED,
+    BROKEN
+}
---
+++ 06Code/CoffeeShop/src/main/java/ec/espe/edu/coffeeshop/repository/EquipmentRepository.java
@@ -0,0 +1,11 @@
+package ec.espe.edu.coffeeshop.repository;
+
+import ec.espe.edu.coffeeshop.model.Equipment;
+
+/**
+ * Repository interface for Equipment entities.
+ * 
+ * @author Kevin Albán, MKA Programmer, @ESPE
+ */
+public interface EquipmentRepository extends Repository<Equipment> {
+}
